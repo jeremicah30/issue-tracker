074d70a458d95c18b4676a4790b857e2cfc0ff53
jeremicah30 <maigabie17@gmail.com>
2024-01-07 16:15:45 +0800

    New issue page component and added font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
+import "./theme-config.css";
 import NavBar from "./NavBar";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Issue Tracker",
@@ -19,11 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Theme accentColor="iris">
           <NavBar />
-          <main>{children}</main>
+          <main className="p-5">{children}</main>
+          <ThemePanel />
         </Theme>
       </body>
     </html>
